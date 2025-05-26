951c9aba84472a5da3f060b861ad081971173c8c
JimyMa <hit16s105116@gmail.com>
2025-05-26 10:55:28 +0800

    format code

--- bench/python/sendrecv_bench.py
+++ bench/python/sendrecv_bench.py
@@ -4,49 +4,26 @@ import os
 import torch
 import torch.distributed as dist
 
-from dlslime import _slime_torch  # noqa: F401
-
-default_sizes = [
-    256,
-    512,
-    1024,
-    2048,
-    4096,
-    8192,
-    16384,
-    32768,
-    65536,
-    131072,
-    262144,
-    524288,
-    1048576,
-    2097152,
-    4194304,
-    8388608,
-    16777216,
-    33554432,
-    67108864,
-    134217728,
-]
+# add dlslime backend
+try:
+    from dlslime import _slime_torch  # noqa: F401
+except ImportError as e:
+    print(e.with_traceback(), 'please install dlslime backend first')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mode', type=str, choices=['send', 'recv'])
-
-parser.add_argument('--master-addr', type=str, default='localhost')
-parser.add_argument('--master-port', type=str, default='6006')
-
-parser.add_argument('--size', nargs='+', type=int, default=default_sizes)
+parser.add_argument('--size', nargs='+', type=int, default=[2 << n for n in range(8, 27)])
 
 args = parser.parse_args()
 
-os.environ['MASTER_ADDR'] = args.master_addr
-os.environ['MASTER_PORT'] = args.master_port
+rank = int(os.environ['RANK'])
+world_size = int(os.environ['WORLD_SIZE'])
+master_addr = os.environ['MASTER_ADDR']
+master_port = os.environ['MASTER_PORT']
+
+assert world_size == 2
 
 print('initializing process group')
-if args.mode == 'send':
-    dist.init_process_group('cuda:dlslime', rank=0, world_size=2)
-if args.mode == 'recv':
-    dist.init_process_group('cuda:dlslime', rank=1, world_size=2)
+dist.init_process_group('cuda:dlslime', rank=rank, world_size=2)
 print('initializing process group done')
 
 start_event = torch.cuda.Event(enable_timing=True)
@@ -59,9 +36,9 @@ for size in args.size:
     ttensor = torch.ones([size]).cuda()
     for _ in range(n_runs):
         start_event.record()
-        if args.mode == 'send':
+        if rank == 0:
             dist.send(ttensor, dst=1)
-        elif args.mode == 'recv':
+        elif rank == 1:
             dist.recv(ttensor, src=0)
         end_event.record()
 
@@ -70,7 +47,8 @@ for size in args.size:
 
         total_time += elapsed_time
 
-    print(f'size: {size}')
-    print(f'total transport: {n_runs * size * ttensor.itemsize}')
-    print(f'average latency: {total_time / n_runs}ms')
-    print(f'bw: {n_runs * size * ttensor.itemsize / total_time / 1e3}MBps')
+    if rank == 1:
+        print(f'size: {size}')
+        print(f'total transport: {n_runs * size * ttensor.itemsize}')
+        print(f'average latency: {total_time / n_runs}ms')
+        print(f'bw: {n_runs * size * ttensor.itemsize / total_time / 1e3} MBps')
