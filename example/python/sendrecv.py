6e031c901e4aa098a01c2686a9a2011382dc0851
JimyMa <33408125+JimyMa@users.noreply.github.com>
2025-07-25 15:17:47 +0800

    Add coalescing (#6)
    * add coalescing
    
    * make cmake more robust
    
    * remove libtorch_cpu linking
    
    * refine cmake
    
    * add adaptive gid index
    
    * 1. refine code
    
    * 1. remove redundant comment
    
    * 1. right refer => left for correct arg parse.
    
    * format document

--- example/python/sendrecv.py
+++ example/python/sendrecv.py
@@ -3,6 +3,7 @@ import os
 
 import torch
 import torch.distributed as dist
+from torch.distributed import distributed_c10d
 
 from dlslime import _slime_torch  # noqa: F401
 
@@ -17,13 +18,39 @@ args = parser.parse_args()
 os.environ['MASTER_ADDR'] = args.master_addr
 os.environ['MASTER_PORT'] = args.master_port
 
-if args.mode == 'send':
-    stensor = torch.ones([1600]).cuda()
-    dist.init_process_group('cuda:dlslime', rank=0, world_size=2)
-    dist.send(stensor, dst=1)
-if args.mode == 'recv':
-    rtensor = torch.zeros([1600]).cuda()
-    dist.init_process_group('cuda:dlslime', rank=1, world_size=2)
-    print(f'rtensor befors recv: {rtensor}')
-    dist.recv(rtensor, src=0)
-    print(f'rtensor after send: {rtensor}')
+rank = 0 if args.mode == 'send' else 1
+dist.init_process_group('cuda:dlslime', rank=rank, world_size=2)
+torch.cuda.set_device(rank)
+
+device = 'cuda'
+send_batch = [torch.ones(3, device=device) * i for i in range(5)]
+recv_batch = [torch.zeros(3, device=device) for _ in range(5)]
+
+reqs = []
+for i in range(5):
+    dst = (rank + 1) % dist.get_world_size()
+    src = (rank - 1) % dist.get_world_size()
+
+    send_op = distributed_c10d.P2POp(dist.isend, send_batch[i], dst, tag=i)
+    recv_op = distributed_c10d.P2POp(dist.irecv, recv_batch[i], src, tag=i)
+
+    reqs.extend([send_op, recv_op])
+
+work = distributed_c10d.batch_isend_irecv(reqs)
+print(work)
+[w.wait() for w in work]
+
+print(f'rtensor after send: {recv_batch}')
+
+dist.destroy_process_group()
+
+# if args.mode == 'send':
+#     stensor = torch.ones([1600]).cuda()
+#     dist.init_process_group('cuda:dlslime', rank=0, world_size=2)
+#     dist.send(stensor, dst=1)
+# if args.mode == 'recv':
+#     rtensor = torch.zeros([1600]).cuda()
+#     dist.init_process_group('cuda:dlslime', rank=1, world_size=2)
+#     print(f'rtensor befors recv: {rtensor}')
+#     dist.recv(rtensor, src=0)
+#     print(f'rtensor after send: {rtensor}')
