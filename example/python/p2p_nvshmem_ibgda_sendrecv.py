997dffb408ea4de5438fe3e94e2432748d95f777
JimyMa <33408125+JimyMa@users.noreply.github.com>
2025-09-12 21:23:03 +0800

    1. nvshmem add coalescing load 2. format code (#23)

--- example/python/p2p_nvshmem_ibgda_sendrecv.py
+++ example/python/p2p_nvshmem_ibgda_sendrecv.py
@@ -1,5 +1,5 @@
+import argparse
 import os
-
 import time
 
 import torch
@@ -8,17 +8,15 @@ import torch.utils.dlpack
 
 from dlslime import _slime_c
 
-import argparse
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--rank", type=int, required=True)
-parser.add_argument("--world-size", type=int, required=True)
+parser.add_argument('--rank', type=int, required=True)
+parser.add_argument('--world-size', type=int, required=True)
 args = parser.parse_args()
 
-os.environ["MASTER_ADDR"] = "127.0.0.1"
-os.environ["MASTER_PORT"] = "6006"
+os.environ['MASTER_ADDR'] = '127.0.0.1'
+os.environ['MASTER_PORT'] = '6006'
 
-dist.init_process_group("cpu:gloo", rank=args.rank, world_size=args.world_size)
+dist.init_process_group('cpu:gloo', rank=args.rank, world_size=args.world_size)
 
 os.environ['NVSHMEM_DISABLE_P2P'] = '1'
 os.environ['NVSHMEM_IB_ENABLE_IBGDA'] = '1'
@@ -43,20 +41,15 @@ dist.all_gather_object(object_list, nvshmem_edpt_info)
 
 nvshmem_ctx.connect_full_mesh(object_list, 0)
 
-local_dlpack_tensor = nvshmem_ctx.alloc_dlpack_tensor(2048, 2048)
-local_tensor = torch.from_dlpack(local_dlpack_tensor)
-if args.rank == 0:
-    local_tensor.add_(1)
+# local_dlpack_tensor = nvshmem_ctx.alloc_dlpack_tensor(16384, 2048)
+# local_tensor = torch.from_dlpack(local_dlpack_tensor)
+local_tensor = torch.ones([16384]).cuda() * args.rank
 print(local_tensor)
 
-nvshmem_ctx.register_memory_region(
-    "buffer",
-    local_tensor.data_ptr(),
-    local_tensor.storage_offset(),
-    local_tensor.numel() * local_tensor.itemsize
-)
+nvshmem_ctx.register_memory_region('buffer', local_tensor.data_ptr(), local_tensor.storage_offset(),
+                                   local_tensor.numel() * local_tensor.itemsize)
 
-print(f"{args.rank=}, {sum(local_tensor)}")
+print(f'{args.rank=}, {sum(local_tensor)}')
 
 start_event = torch.cuda.Event(enable_timing=True)
 end_event = torch.cuda.Event(enable_timing=True)
@@ -64,20 +57,20 @@ end_event = torch.cuda.Event(enable_timing=True)
 start_event.record()
 for i in range(10):
     if args.rank == 0:
-        print("Data Sending")
-        nvshmem_ctx.send("buffer", 1)
-        print(f"{args.rank=}, {sum(local_tensor.to(torch.float32))}")
+        print('Data Sending')
+        nvshmem_ctx.send('buffer', 1)
+        print(f'{args.rank=}, {sum(local_tensor.to(torch.float32))}')
     else:
-        print("Data Recving")
-        nvshmem_ctx.recv("buffer", 0)
+        print('Data Recving')
+        nvshmem_ctx.recv('buffer', 0)
         print(local_tensor)
-        print(f"{args.rank=}, {sum(local_tensor.to(torch.float32))}")
-torch.cuda.synchronize()    
+        print(f'{args.rank=}, {sum(local_tensor.to(torch.float32))}')
+    torch.cuda.synchronize()
 end_event.record()
 
 print(start_event.elapsed_time(end_event))
 torch.cuda.synchronize()
 time.sleep(1)
 
-print("done")
+print('done')
 dist.barrier()
