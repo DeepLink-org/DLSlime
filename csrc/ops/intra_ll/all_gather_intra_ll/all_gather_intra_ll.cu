e41a60a276e0db2ac9c9189cd908cd4f161e7edb
JimyMa <hit16s105116@gmail.com>
2025-10-18 07:13:06 +0000

    add lint

--- csrc/ops/intra_ll/all_gather_intra_ll/all_gather_intra_ll.cu
+++ csrc/ops/intra_ll/all_gather_intra_ll/all_gather_intra_ll.cu
@@ -27,11 +27,10 @@ __global__ __launch_bounds__(1024, 1) void all_gather_intra_ll_kernel(int8_t*  q
                                                                       int32_t  itemsize,
                                                                       int32_t  world_size,
                                                                       int32_t  rank,
-                                                                      int32_t* mask = nullptr
-                                                                    )
+                                                                      int32_t* mask = nullptr)
 {
 
-    const int num_sms = world_size;
+    const int num_sms   = world_size;
     const int num_warps = std::min(32, max_bs);
 
     const int sm_id   = blockIdx.x;
@@ -44,34 +43,33 @@ __global__ __launch_bounds__(1024, 1) void all_gather_intra_ll_kernel(int8_t*  q
     using vec_t        = int4;
     const int VEC_SIZE = sizeof(int4);
 
-    const int q_size     = max_bs * msg_size * itemsize;
+    const int q_size = max_bs * msg_size * itemsize;
 
     const int num_msg_per_warp     = msg_size * itemsize;
     const int num_vec_msg_per_warp = num_msg_per_warp / VEC_SIZE;
 
-    for (int msg_idx = warp_id; msg_idx < max_bs; msg_idx += num_warps)
-    {
+    for (int msg_idx = warp_id; msg_idx < max_bs; msg_idx += num_warps) {
         int8_t*   q_ptr_for_write          = q_ptr + msg_idx * num_msg_per_warp;
         vec_t*    vec_q_ptr_for_write      = reinterpret_cast<vec_t*>(q_ptr_for_write);
         const int buffer_idx               = rank * q_size + msg_idx * num_msg_per_warp;
         int8_t*   buffer_ptr_for_write     = ipc_buffer_ptr[sm_id] + buffer_idx;
         vec_t*    vec_buffer_ptr_for_write = reinterpret_cast<vec_t*>(buffer_ptr_for_write);
 
-        if(!mask or __ldg(mask + msg_idx * world_size + sm_id)) {
+        if (!mask or __ldg(mask + msg_idx * world_size + sm_id)) {
             UNROLLED_WARP_COPY(8,
-                lane_id,
-                num_vec_msg_per_warp,
-                vec_buffer_ptr_for_write,
-                vec_q_ptr_for_write,
-                deep_ep::ld_nc_global,
-                deep_ep::st_na_global);
+                               lane_id,
+                               num_vec_msg_per_warp,
+                               vec_buffer_ptr_for_write,
+                               vec_q_ptr_for_write,
+                               deep_ep::ld_nc_global,
+                               deep_ep::st_na_global);
         }
     }
 
     __syncthreads();
 
     int* signal_ptr = ipc_signal_ptr[sm_id];
-    lane_id == 0 and warp_id == 0? deep_ep::atomic_add_release_global(signal_ptr + rank, 1): 0;
+    lane_id == 0 and warp_id == 0 ? deep_ep::atomic_add_release_global(signal_ptr + rank, 1) : 0;
 
     // Step 3. sync
     int* local_signal_ptr = ipc_signal_ptr[rank];
@@ -80,25 +78,24 @@ __global__ __launch_bounds__(1024, 1) void all_gather_intra_ll_kernel(int8_t*  q
             ;
         local_signal_ptr[threadIdx.x] = 0;
     }
-
 }
 
-void all_gather_intra_ll(torch::Tensor q,
-                         int8_t**      ipc_buffer_ptr,
-                         int**         ipc_signal_ptr,
-                         int32_t       max_bs,
-                         int32_t       msg_size,
-                         int32_t       itemsize,
-                         int32_t       world_size,
-                         int32_t       rank,
+void all_gather_intra_ll(torch::Tensor                q,
+                         int8_t**                     ipc_buffer_ptr,
+                         int**                        ipc_signal_ptr,
+                         int32_t                      max_bs,
+                         int32_t                      msg_size,
+                         int32_t                      itemsize,
+                         int32_t                      world_size,
+                         int32_t                      rank,
                          c10::optional<torch::Tensor> mask)
 {
 
     int8_t* q_ptr = reinterpret_cast<int8_t*>(q.data_ptr());
 
-    int32_t* mask_ptr = mask.has_value()? (*mask).data_ptr<int32_t>(): nullptr;
+    int32_t* mask_ptr = mask.has_value() ? (*mask).data_ptr<int32_t>() : nullptr;
 
-    int num_sms = world_size;
+    int num_sms   = world_size;
     int num_warps = std::min(32, max_bs);
 
     int grid_dim  = num_sms;
